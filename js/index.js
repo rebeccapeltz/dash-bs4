501585a8bcbc9fa0dfeae64053cebb6d6619049e
rebeccapeltz <rebeccapeltz@gmail.com>
2017-09-24 17:47:52 -0400

    adjust colors; add multiple theme selection

--- js/index.js
+++ js/index.js
@@ -110,4 +110,40 @@ $(window).on("load", function(){
             ]
             }]
         });
+  
+  /* On load, set theme from local storage */
+if (supports_storage) {
+  var theme = localStorage.theme;
+  if (theme) {
+    set_theme(theme);
+  }
+} else {
+  /* Don't annoy user with options that don't persist */
+  $('#theme-dropdown').hide();
+}
     });
+
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
+var supports_storage = supports_html5_storage();
+
+function set_theme(theme) {
+  $('link[title="main"]').attr('href', theme);
+   if (supports_storage) {
+    localStorage.theme = theme;
+  }
+}
+
+jQuery(function($) {
+    $('body').on('click', '.change-style-menu-item', function() {
+      var theme_name = $(this).attr('rel');
+      var theme = "vendor/bootstrap/" + theme_name + "/bootstrap.min.css";
+      set_theme(theme);
+    });
+});
